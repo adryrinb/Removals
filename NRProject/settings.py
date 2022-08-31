9281755a3415594f49bff1e69238f8a70983f2f9
Adriana Rincón <adryrinb@hotmail.com>
2022-08-31 10:55:18 -0500

    corrigiendo estilos al menú

--- NRProject/settings.py
+++ NRProject/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nationwidemovers.ap-southeast-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,23 +128,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+#GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
 
 # Email conf
-EMAIL_HOST = os.environ['NWM_EMAIL_HOST']
-EMAIL_PORT = os.environ['NWM_EMAIL_PORT']
-EMAIL_HOST_USER = os.environ['NWM_EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['NWM_EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+#EMAIL_HOST = os.environ['NWM_EMAIL_HOST']
+#EMAIL_PORT = os.environ['NWM_EMAIL_PORT']
+#EMAIL_HOST_USER = os.environ['NWM_EMAIL_HOST_USER']
+#EMAIL_HOST_PASSWORD = os.environ['NWM_EMAIL_HOST_PASSWORD']
+#EMAIL_USE_TLS = False
+#EMAIL_USE_SSL = True
 
 # Security
 # SECURE_SSL_REDIRECT = True
